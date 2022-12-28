a83a6f08f28d93bb27cc2e7409bd4167de7434da
Marvin <mjae0109@gmail.com>
2022-12-28 22:42:59 +0800

    readMe updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# machineLearningPractice
+# Machine Learning Practice Repository
 This is my repository for learning about machine learning.
+
+## Linear Regression
+Program's File Name: linearRegression.py
+Data Used File Name: student-mat.csv
+Source Link for Data File: [UCI's Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/Student+Performance)
+
+## K-Nearest Neighbor Algorithm 
+Program's File Name: knn.py
+Data Used File Name: car.data
+Source Link for Data File: [UCI's Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/car+evaluation)
--- knn.py
+++ knn.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import linear_model, preprocessing
+from sklearn import linear_model, preprocessing, model_selection
 
 # Reading from file
 data = pd.read_csv('car.data')
 
-# Preprocessing data [transforming str to int]
+# Preprocessing data [transforming str to int then placing them in a list]
 
 label = preprocessing.LabelEncoder()
 
@@ -21,4 +21,13 @@ persons = labeler('persons')
 lug_boot = labeler('lug_boot')
 safety = labeler('safety')
 
-print(safety)
+# Storing them in a class
+
+predict = 'class'
+
+x = list(zip(buying, maint, doors, persons, lug_boot, safety))
+y = list(predict)
+
+# Splitting attributes for Testing
+
+xTrain, xTest, yTrain, yTest = model_selection.train_test_split(x, y, test_size=0.1)
